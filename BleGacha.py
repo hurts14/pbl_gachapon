3e288a22483e61bc375ee26e20f8c76e94d5dace
hurts14 <bayern.uver@gmail.com>
2020-08-18 19:44:22 +0900

    add apply gacha

--- BleGacha.py
+++ BleGacha.py
@@ -4,12 +4,16 @@
 import time
 import subprocess
 import sys
-#from beacontools import BeaconScanner, EddystoneTLMFrame, EddystoneFilter
-from beacontools import BeaconScanner, IBeaconFilter, IBeaconAdvertisement, BluetoothAddressType, ExposureNotificationFrame
+from beacontools import BeaconScanner, BluetoothAddressType, ExposureNotificationFrame
 
 bt_list={}
+
 def callback(bt_addr, rssi, packet, additional_info):
-    print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
+    try:
+        print("<%s, %d> %s %s" % (bt_addr, rssi, packet, additional_info))
+    
+    except:
+        print(sys.exc_info())
 
 
 def moveGacha():
@@ -22,19 +26,26 @@ def scan_cocoa():
     
     scanner = BeaconScanner(callback,
                         # remove the following line to see packets from all beacons
-                        #device_filter=IBeaconFilter(uuid="0000fd6f-0000-1000-8000-00805f9b34fb"),#[0xFD6F]
-                        #packet_filter=IBeaconAdvertiesment,
                         packet_filter=[ExposureNotificationFrame],
                         scan_parameters={'address_type':BluetoothAddressType.PUBLIC}
                        )
-
     scanner.start()
-    time.sleep(10)
-    scanner.stop()
+    try:
+        while True:
+            if(0 == len(bt_list)):
+                print('<No User Installed Cocoa app>')
+            for i in list(bt_list):
+                #新しい配列かつ強度が70以上なら
+                if(True):
+                    print('')
+                    moveGacha()
+                else:
+                    #すでにある
+                    print('')
+    #time.sleep(10)
+    except KeyboardInterrupt:
+        scanner.stop()
 
-
-def scan_ble():
-    print('yes')
 
 def main():
     scan_cocoa()
